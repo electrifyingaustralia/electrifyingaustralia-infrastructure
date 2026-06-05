133bd3f85ebfe2845f61d2cfa2c1387ae76889cb
Electrifying Australia <138639423+electrifyingaustralia@users.noreply.github.com>
2026-06-05 10:38:08 +0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "instance_id" {
+  default = "i-05a1f385c12b518f5"
+}
+
+variable "vpc_id" {
+  default = "vpc-02ba2f30a14141290"
+}
+
+variable "subnet_id" {
+  default = "subnet-098d9a2889454a80e"
+}
+
+variable "eip_allocation_id" {
+  default = "eipalloc-052befb97d5385105"
+}
+
+variable "sg_id" {
+  default = "sg-0040054cb48638569"
+}
+
+variable "s3_bucket" {
+  default = "electrifying-australia"
+}
+
+variable "region" {
+  default = "ap-southeast-1"
+}
