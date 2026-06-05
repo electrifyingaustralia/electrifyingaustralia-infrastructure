21a52dab089d0a2c5c2491deaa376d58d159e0f5
Electrifying Australia <138639423+electrifyingaustralia@users.noreply.github.com>
2026-06-05 10:39:36 +0600

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "main" {
+  bucket = var.s3_bucket
+
+  tags = {
+    Name        = "electrifying-australia"
+    Environment = "production"
+    Project     = "electrifying-australia"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${var.s3_bucket}/*"
+      }
+    ]
+  })
+}
